ad87ee4d7ef3556a3a96923e7c4a172fec4ef002
Enzo <enzzzooo@proton.me>
2025-12-04 12:34:50 +0000

    fix, make average rooms per house from total rooms in one census block

--- final.py
+++ final.py
@@ -5,25 +5,30 @@ import sys
 sys.path.insert(0, "./data-science-from-scratch-master")
 from scratch.simple_linear_regression import *
 
+with open('./housing.csv', 'r') as f:
+    reader = csv.reader(f)
+# skip header row
+    next(reader)  
 # kNN Task: scale numerical features; apply kNN regression to predict median house value.
 
 
 
 # Linear regression: simple linear regression predicting house value from a single key feature (e.g., averagerooms).
-with open('./housing.csv', 'r') as f:
-    reader = csv.reader(f)
-# skip header row
-    next(reader)  
+
 # ninth column
     median_house_value = []
 # fourth column
-    total_rooms = []
+    average_rooms_per_house = []
 # read file
     for row in reader:
+        households = float(row[6])
+        if households == 0:
+            continue  # avoid division by zero
         median_house_value.append(float(row[8]))
-        total_rooms.append(float(row[3]))
+        average_rooms_per_house.append(float(row[3]) / households)
 
-alpha, beta = least_squares_fit(median_house_value, total_rooms)
+
+alpha, beta = least_squares_fit(median_house_value, average_rooms_per_house)
 
 print(f"Dataset generated from housing.csv")
 print(f"---")
@@ -31,10 +36,10 @@ print(f"Calculated Y-Intercept (alpha or b): {alpha}")
 print(f"Calculated Slope (beta or m): {beta}")
 
 plt.figure()
-plt.scatter(median_house_value, total_rooms, color="red", edgecolors="black", zorder=3, label="Housing")
+plt.scatter(median_house_value, average_rooms_per_house, color="red", edgecolors="black", zorder=3, label="Housing")
 plt.title("Housing Data")
 plt.xlabel("Median House Value")
-plt.ylabel("Total Rooms")
+plt.ylabel("Average Rooms per Household")
 # best fit line
 median_house_value = [min(median_house_value), max(median_house_value)]
 y_line = [predict(alpha, beta, xi) for xi in median_house_value]
@@ -54,10 +59,10 @@ print("Plot saved as LinearRegressionScatter.png")
 
 # Output: data pre–processing, model performance metrics (RMSE, R²), and interpretation
 
-sse = sum_of_sqerrors(alpha, beta, median_house_value, total_rooms)
+sse = sum_of_sqerrors(alpha, beta, median_house_value, average_rooms_per_house)
 print(f"\n3. sum_of_sqerrors() (SSE): {sse}")
 
-r_sq = r_squared(alpha, beta, median_house_value, total_rooms)
+r_sq = r_squared(alpha, beta, median_house_value, average_rooms_per_house)
 print(f"4. r_squared() (r2): {r_sq}")
 
 
